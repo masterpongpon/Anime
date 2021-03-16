92f0a1b45dd0c56902fc4fb7a63acb614aa81aff
masterpongpon <kira_songpon@hotmail.com>
2021-03-16 22:11:40 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,67 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav id="nav">
+        <router-link to="/">Home</router-link> 
+      </b-navbar-nav>
+      <b-nav-form class="offset-md-10">
+        <b-nav-input
+          size="sm"
+          class="mr-sm-2 col-8"
+          placeholder="Search"
+          v-model="sh"
+        ></b-nav-input>
+        <b-button size="sm" class="my-2-my-sm-0" type="submit" @click="Search()"
+          >Search</b-button
+        >
+      </b-nav-form>
+    </b-navbar>
     <router-view/>
+   
   </div>
 </template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      sh: "",
+      animeList: null,
+      url: "",
+    };
+  },
+  methods: {
+    Search() {
+      const router = this.$router;
+      for (let i = 0; i <= this.animeList.anime.length; i++) {
+        if (this.sh == this.animeList.anime[i].title) {
+          alert(this.animeList.anime[i].title);
+          router.push({
+            path: `/anime/${this.animeList.anime[i].mal_id}`,
+          });
+          break;
+        } else if (99 == i) {
+          router.push({
+            path: `/`,
+          });
+        }
+      }
+    },
+  },
+  mounted() {
+    this.url = "https://api.jikan.moe/v3/producer/1/1";
+    axios
+      .get(this.url)
+      .then((result) => {
+        this.animeList = result.data;
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      });
+  },
+};
+</script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <b-container>
+      <br />
+      <b-row>
+      <MainPage
+        v-for="(item, index) in animeList.anime"
+        :key="index"
+        :Name="item.title"
+        :img="item.image_url"
+        :id="item.mal_id"
+      />
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import axios from "axios";
+import MainPage from "@/components/MainPage.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    MainPage,
+  },
+  props: {
+    page: Number,
+  },
+  data() {
+    return {
+      animeList: null,
+      url: "https://api.jikan.moe/v3/producer/1/1",
+    };
+  },
+  mounted() {
+    axios
+      .get(this.url)
+      .then((result) => {
+        this.animeList = result.data;
+      })
+      .catch((err) => {
+        console.log(err);
+        alert(err);
+      });
+  },
+};
 </script>
